3904e1de7ff70a1fef490e68e28d6c758e4be542
Toxichys <ToxichSpam@bk.ru>
2022-01-16 16:28:01 +0200

    Finished
    Finished

---
+++ MyHomework_Lesson_4/MyHomework_Lesson_4_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyHomework_Lesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array = new string[,]
+            {
+                {"Ivanov", "Ivan", "Ivanovich"},
+                {"Petrov", "Petr", "Petrovich"},
+                {"Sidorov", "Maksim", "Karlovich"}
+            };
+            //string fullName1 = GetFullName("Ivanov", "Ivan", "Ivanovich");
+            for (int i = 0; i < 3; i++)
+            {
+                string fullName1 = GetFullName(array[i, 0], array[i, 1], array[i, 2]);
+                System.Console.WriteLine(fullName1);
+            }
+            //string fullName2 = GetFullName("Petrov", "Petr", "Petrovich");
+            //System.Console.WriteLine(fullName2);
+            //string fullName3 = GetFullName("Sidorov", "Maksim", "Karlovich");
+            //System.Console.WriteLine(fullName3);
+
+            System.Console.ReadLine();
+        }
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string fullName = firstName + " " + lastName + " " + patronymic;
+            return fullName;
+        }
+    }
+}
---
+++ MyHomework_Lesson_4/MyHomework_Lesson_4_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MyHomework_Lesson_4_3
+{
+    class Program
+    {
+        enum Season
+        {
+            Winter = 1,
+            Spring = 2,
+            Summer = 3,
+            Autumn = 4
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the month number!");
+            int nMonth = Convert.ToInt32(Console.ReadLine());
+            Season userSeason = GetSeason(nMonth);
+            string textTimeYear = GetNameSeason(userSeason);
+            Console.WriteLine(textTimeYear);
+            Console.ReadLine();
+
+        }
+        static Season GetSeason(int nMonth)
+        {
+            Season userSeason = new Season();
+            switch (nMonth)
+            {
+                case 12:
+                case 1:
+                case 2: userSeason = Season.Winter; break;
+                case 3:
+                case 4:
+                case 5: userSeason = Season.Spring; break;
+                case 6:
+                case 7:
+                case 8: userSeason = Season.Summer; break;
+                case 9:
+                case 10:
+                case 11: userSeason = Season.Autumn; break;
+            }
+            return userSeason;
+        }
+        static string GetNameSeason(Season userSeason)
+        {
+            string textTimeYear = "";
+            switch (userSeason)
+            {
+                case Season.Autumn: textTimeYear = "Autumn";break;
+                case Season.Spring: textTimeYear = "Spring"; break;
+                case Season.Summer: textTimeYear = "Summer"; break;
+                case Season.Winter: textTimeYear = "Winter"; break;
+                default: textTimeYear = "Error: Enter a number from 1 to 12!"; break;
+            }
+            return textTimeYear;
+        }
+    }
+}
