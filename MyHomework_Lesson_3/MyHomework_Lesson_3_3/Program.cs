3427bacc73f149c54f7f488d6e9e78265c168636
Toxichys <ToxichSpam@bk.ru>
2022-01-18 00:54:17 +0200

    Finished
    Finished

---
+++ MyHomework_Lesson_3/MyHomework_Lesson_3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyHomework_Lesson_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).
+            Console.WriteLine("Введите строку:");
+            string text = Console.ReadLine();
+            for (int i=1; i <= text.Length;i++)
+            {
+                Console.Write(text[text.Length - i]);
+            }
+        }
+    }
+}
